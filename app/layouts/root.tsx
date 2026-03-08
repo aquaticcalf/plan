81c2bf84a461807a7334f245f29a07ecf5f67b19
sam <aquaticcalf@proton.me>
2026-03-08 12:35:33 +0530

    layout props type

--- app/layouts/root.tsx
+++ app/layouts/root.tsx
@@ -1,3 +1,5 @@
-export default function RootLayout(props) {
+import type { RouteSectionProps } from "@solidjs/router"
+
+export default function RootLayout(props: RouteSectionProps) {
   return <div class="rootlayout">{props.children}</div>
 }
